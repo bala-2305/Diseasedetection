1709d361e470a3a76e093ceece9f0aa04d6af671
Mahibala <hmahibala@gmail.com>
2025-08-04 21:42:14 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("📄 Extra Information")
+st.write("This page gives more details about plant diseases or app usage.")
+st.markdown("👉 Use the sidebar to go back to the main prediction page.")
